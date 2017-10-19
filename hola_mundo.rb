8f219672dcad45a2b9cf893f0da5f27f86f3d2de
avanegasp <arco527@gmail.com>
2017-10-18 20:11:09 -0500

    reduce, filter, each

--- hola_mundo.rb
+++ hola_mundo.rb
@@ -1,5 +1,21 @@
-def longitud (palabra)
-   palabra.length
+
+#
+# nums = [1,2,3,4]
+#
+# resultado = []
+#
+# nums.each do |element|
+# resultado = resultado << element*2
+#
+# end
+#
+# p resultado
+
+
+nums = [1,2,3,4]
+
+mult = nums.map do |element|
+  element * 2
 end
 
-longitud ("hola")
+  p mult
