892e816584fe627391cdff46521f12ee4053beb7
avanegasp <arco527@gmail.com>
2017-10-22 21:38:06 -0500

    Resolving exercises

--- array.rb
+++ array.rb
@@ -1,8 +1,9 @@
 # nums = [1,2,3,4]
-
-# mult = nums.map do |element|
-#   element * 2
-# end
+# # def multiplier (nums,num)
+# mult=[]
+# # nums.map do |element|
+# #  mult <<  element * num
+# # end
 #
 # p mult
 #
@@ -44,13 +45,99 @@
 #
 # p santa_claus(1)
 
-def santa_claus(num)
-  word = ""
-  var_jo = "jo"
-  if num == 0
-    ""
-  else
-    word << var_jo * num
-  end
-end
-p santa_claus(0)
+# def santa_claus(num)
+#   word = ""
+#   var_jo = "jo"
+#   if num == 0
+#     ""
+#   else
+#     word << var_jo * num
+#   end
+# end
+# p santa_claus(0)
+
+
+# def adjust_text(cadena, num)
+#   reducir = ""
+#     if cadena < num
+#       reducir = cadena.size - num
+#     end
+# end
+# adjust_text("hola", 8)
+
+# def multiplier (array, num)
+#   resultado = []
+#     array.each do |elem|
+#   resultado << elem * num
+#   end
+#   resultado
+# end
+# p multiplier([1,2,3,], 2)
+
+
+
+# def multiplier (nums,num)
+# mult=[]
+#   nums.map do |element|
+#   mult <<  element * num
+#  end
+# end
+# p multiplier([1, 2, 3], 10)
+# #
+# #
+# #
+#  #
+#  def multiplier(array, numer)
+#  nums_times_two = []
+#
+#     array.each do |num|
+#     nums_times_two << (num *  numer)
+#    end
+#  end
+
+
+#  nums = [1,2,3,4,5,6]
+#  nums_times_two = []
+#
+#  nums.each do |num|
+#    nums_times_two << (num *  2)
+#  end
+# p nums_times_two
+
+
+# def multiplier(nums, num)
+# mult = nums.map do |element|
+#     element * num
+#   end
+# end
+#
+# p m ultiplier()
+
+# def multiplier(arr, num)
+#  mult = arr.map do |element|
+#     element * num
+#   end
+# end
+# p multiplier([1, 2, 3], 10)
+
+# def say_hello_to_person?(name)
+#   # Guard Clause
+#   return "chao" if name == "Maduro"
+#   "Hola como estas"
+# end
+#
+# p say_hello_to_person?("Pedro")
+
+#
+# def alive?(playerName, points)
+#   if playerName == "ikk" && points > 30 || playerName == "gut" && points >10
+#     true
+#   else
+#     false
+#   end
+# end
+# p alive?("ikk", 50) #=> true
+# p alive?("gut", 50) #=> true
+# p alive?("ikk", 20) #=> false
+# p alive?("gut", 5) #=> false
+# p alive?("trek", 50) #=> false
